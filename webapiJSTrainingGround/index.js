db5d1fa002a69974cf61d80f5477cd2d5edbc32f
lukas-laas <111971517+lukas-laas@users.noreply.github.com>
2024-05-21 12:36:00 +0200

    add parameter for getting devloper by id

--- webapiJSTrainingGround/index.js
+++ webapiJSTrainingGround/index.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const app = express()
 
-app.get("/", (req, res) => {
-    const dev = {
+const db = [
+    {
         id: 1,
         name: "Lukas Dev",
         email: "lukas@snabela.se",
+    },
+    {
+        id: 2,
+        name: "Lukas Dev2",
+        email: "lukas@snabella.se",
     }
-    res.json(dev)
+]
+
+app.get("/api/developers", (req, res) => {
+    res.json(db)
 })
+
+app.get('/api/developers/:id', (req, res) => {
+    const dev = db.find(dev => dev.id == req.params.id);
+
+    return dev ? res.json(dev) : res.status(404).end;
+});
 
 const port = 3000
 app.listen(port, () => {
