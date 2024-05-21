64bb02b3fdc570ea1e9aeeb0f4de77b289e5a3eb
lukas-laas <111971517+lukas-laas@users.noreply.github.com>
2024-05-21 11:15:09 +0200

    add method for parsing url and methods for handling missing favicon

--- webapiJSTrainingGround/index.js
+++ webapiJSTrainingGround/index.js
@@ -1,8 +1,27 @@
 const http = require("http");
 const fs = require("fs");
 
+const fileNameOfUrl = url => {
+    let fileName = '';
+    if (url.split('/')[1] === '') {
+        fileName = 'index.html'
+    } else {
+        fileName = url.split('/')[1]
+    }
+    return fileName;
+}
+
 const server = http.createServer((req, res) => {
-    const content = fs.readFileSync("./static/index.html", "utf-8")
+    console.log(`The URL for the request was '${req.url}'`);
+    console.log(`The Method for the request was '${req.method}'`);
+
+    const fileName = fileNameOfUrl(req.url)
+    if (fileName === "favicon.ico") {
+        res.statusCode = 404;
+        res.end("")
+        return;
+    }
+    const content = fs.readFileSync(`./static/${fileName}`, "utf-8")
     res.statusCode = 200
     res.setHeader("Content-Type", "text/html")
     res.end(content)
