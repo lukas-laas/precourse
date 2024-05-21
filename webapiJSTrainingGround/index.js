42497b3d1d8b5c6dfcc30339f10cf25abbd4f16e
lukas-laas <111971517+lukas-laas@users.noreply.github.com>
2024-05-21 11:02:14 +0200

    make server return static html document

--- webapiJSTrainingGround/index.js
+++ webapiJSTrainingGround/index.js
@@ -1,9 +1,11 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+    const content = fs.readFileSync("./static/index.html", "utf-8")
     res.statusCode = 200
     res.setHeader("Content-Type", "text/html")
-    res.end("<h1>Hello fellow developer!<h1/>")
+    res.end(content)
 });
 
 const hostname = "localhost"
