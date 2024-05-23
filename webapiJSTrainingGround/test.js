f875864dbb372d2767120b8530cc52cefa2b6014
lukas-laas <111971517+lukas-laas@users.noreply.github.com>
2024-05-23 12:14:15 +0200

    add test framework

---
+++ webapiJSTrainingGround/test.js
@@ -0,0 +1,8 @@
+const assert = require("assert")
+const request = require("supertest")
+
+describe("developer API should have endpoints to", () => {
+    it("get all developers", () => {
+        assert.strictEqual(false, true)
+    })
+})
