a72b95b0d56e31c213888043e33fc386c4f05613
lukas-laas <111971517+lukas-laas@users.noreply.github.com>
2024-05-24 10:03:10 +0200

    add test for getting all developers

--- webapiJSTrainingGround/test.js
+++ webapiJSTrainingGround/test.js
@@ -2,7 +2,16 @@ const assert = require("assert")
 const request = require("supertest")
 
 describe("developer API should have endpoints to", () => {
-    it("get all developers", () => {
-        assert.strictEqual(false, true)
+    it("get all developers", function (done) {
+        const api = require("./api.js")
+
+        request(api.app)
+            .get("/api/developers")
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect((res) => {
+                assert.strictEqual(res.body.length, 2);
+            })
+            .expect(200, done)
     })
 })
