a08686aa131d9b15639bbb127cdac6b66c0e655b
lukas-laas <111971517+lukas-laas@users.noreply.github.com>
2024-05-24 10:35:50 +0200

    add test for getting developer by id

--- webapiJSTrainingGround/test.js
+++ webapiJSTrainingGround/test.js
@@ -1,9 +1,9 @@
 const assert = require("assert")
 const request = require("supertest")
+const api = require("./api.js")
 
 describe("developer API should have endpoints to", () => {
     it("get all developers", function (done) {
-        const api = require("./api.js")
 
         request(api.app)
             .get("/api/developers")
@@ -11,6 +11,17 @@ describe("developer API should have endpoints to", () => {
             .expect("Content-Type", /json/)
             .expect((res) => {
                 assert.strictEqual(res.body.length, 2);
+            })
+            .expect(200, done)
+    })
+
+    it("get developer by id", function (done) {
+        request(api.app)
+            .get("/api/developers/1")
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect((res) => {
+                assert.strictEqual(res.body.id, 1);
             })
             .expect(200, done)
     })
